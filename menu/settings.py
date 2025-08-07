ee39ccd4534de9230cdf77e1188997252decd1e5
AndisKrams <523ku12i@students.codeinstitute.net>
2025-08-07 11:14:17 +0300

    Fix allowed hosts

--- menu/settings.py
+++ menu/settings.py
@@ -25,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
-    '127.0.0.1:8000',
+    '127.0.0.1',
 ]
 
 
